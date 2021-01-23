cd57700b80f72bc220cdfc76fe59a4626d3b947c
hasyrails <hasyrails@gmail.com>
2021-01-23 16:10:41 +0900

    表示できる動画の情報

--- app/controllers/youtube/videos_controller.rb
+++ app/controllers/youtube/videos_controller.rb
@@ -3,20 +3,20 @@ class Youtube::VideosController < ApplicationController
   GOOGLE_API_KEY = Rails.application.credentials.google[:api_key]
 
   def find_videos(keyword, after: 1.months.ago, before: Time.now)
-    service = Google::Apis::YoutubeV3::YouTubeService.new
-    service.key = GOOGLE_API_KEY
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.key = GOOGLE_API_KEY
 
     next_page_token = nil
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 20,
+      max_results: 2,
       order: :date,
       page_token: next_page_token,
       published_after: after.iso8601,
       published_before: before.iso8601
     }
-    service.list_searches(:snippet, opt)
+    youtube.list_searches(:snippet, opt)
   end
 
   def index
