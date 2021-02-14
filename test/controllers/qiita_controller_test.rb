3808f475e9ccc208566153aee47d03627a738c7d
hasyrails <hasyrails@gmail.com>
2021-02-14 20:53:50 +0900

    testファイル　git rm

--- test/controllers/qiita_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class QiitaControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/controllers/youtube/myvideos_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Youtube::MyvideosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
