09edee9bfc93ef8499e3f439245038ff6efe079c
hasyrails <hasyrails@gmail.com>
2021-02-19 06:56:40 +0900

    dependent: :destroy追記

--- app/models/youtube_video.rb
+++ app/models/youtube_video.rb
@@ -1,6 +1,7 @@
 class YoutubeVideo < ApplicationRecord
-  has_many :favorites
+  has_many :favorites, dependent: :destroy
   has_many :users, through: :favorites
+
   enum status: { default: 0, like: 1, dislike: 2 }
   validates :video_id, uniqueness: true
 end
