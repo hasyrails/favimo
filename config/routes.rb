99b8b75b07f648bcf19c6d3c66b51cef809a659d
hasyrails <hasyrails@gmail.com>
2021-02-21 20:12:03 +0900

    動画レコードをLikeした他のuserを表示成功

--- config/routes.rb
+++ config/routes.rb
@@ -10,13 +10,14 @@ Rails.application.routes.draw do
   resources :reactions, only: [:create]
   resources :matching, only: [:index]
   resources :chat_rooms, only: [:create, :show]
+  namespace :users do
+    get 'video_common_like/:video_unique_id',  to: 'video_common_like#index', as: 'video_common_like'
+  end
   resources :qiitas
   namespace :youtube do
     resources :videos_search
     # resources :myvideos
     get '/myvideos/:q', to: 'myvideos#index', as: 'myvideos'
-    namespace :videos do
-    end
     namespace :myvideos do
       resources :favorites, only: [:create, :update]
       namespace :status do
