4a60dae952b30e90bbb50483fb848e03cab609bc
hasyrails <hasyrails@gmail.com>
2021-02-21 21:02:05 +0900

    検索ワードを表示

--- app/controllers/youtube/videos_search_controller.rb
+++ app/controllers/youtube/videos_search_controller.rb
@@ -60,6 +60,15 @@ class Youtube::VideosSearchController < ApplicationController
       redirect_to youtube_myvideos_path(q: params[:keyword]) and return
     end
 
+    search_words = []
+    @search_words = []
+    youtube_videos = YoutubeVideo.order(:created_at).limit(20)
+
+    youtube_videos.each do |youtube_video|
+      search_word = youtube_video.search_keyword
+      search_words << search_word
+      @search_words = search_words.uniq
+    end
   end
 
   private
