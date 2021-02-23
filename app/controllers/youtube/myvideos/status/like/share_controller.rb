f8dde6e29e1a2a149b8ebe22fc4a248994c18578
hasyrails <hasyrails@gmail.com>
2021-02-24 07:29:36 +0900

    Share機能のコントーラ・ビュー設定

---
+++ app/controllers/youtube/myvideos/status/like/share_controller.rb
@@ -0,0 +1,25 @@
+class Youtube::Myvideos::Status::Like::ShareController < ApplicationController
+  def new
+    @user = current_user
+    @share_video = YoutubeVideo.find(params[:id])
+
+    like_reactions_by_current_user = Reaction.where(from_user_id: current_user.id, status: "like")
+
+    @users_liked_by_current_user = []
+
+    like_reactions_by_current_user.each do |like_reaction|
+      user_liked_by_current_user = User.find(like_reaction.to_user_id)
+      @users_liked_by_current_user << user_liked_by_current_user
+    end
+  end
+
+  def create
+    youtube_video = YoutubeVideo.find_by(video_id: params[:youtube_video_id])
+
+    share = ShareVideo.create!(
+      youtube_video_id: youtube_video.id,
+      from_user_id: params[:from_user_id],
+      to_user_id: params[:to_user_id]
+    )
+  end
+end
