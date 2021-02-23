4ba40afeec93e47a92e6de23bd1d7a071bb36eb1
hasyrails <hasyrails@gmail.com>
2021-02-24 07:21:05 +0900

    Likeした動画の検索機能を追加

--- app/controllers/youtube/myvideos/status/like_controller.rb
+++ app/controllers/youtube/myvideos/status/like_controller.rb
@@ -1,11 +1,16 @@
 class Youtube::Myvideos::Status::LikeController < ApplicationController
   def index
     @like_myvideos = []
-    like_state_records = current_user.favorites.where(status: 'like')
-    like_state_records.each do |like_state_record|
-      like_state_record_id = like_state_record.youtube_video_id
-      like_myvideo = YoutubeVideo.find(like_state_record_id)
-      @like_myvideos << like_myvideo
+    keyword = params[:keyword]
+    if keyword.present?
+      @like_myvideos = current_user.youtube_videos.where(status: 'like').where('title LIKE ?', '%'+keyword+'%')
+    else
+      like_state_records = current_user.favorites.where(status: 'like')
+      like_state_records.each do |like_state_record|
+        like_state_record_id = like_state_record.youtube_video_id
+        like_myvideo = YoutubeVideo.find(like_state_record_id)
+        @like_myvideos << like_myvideo
+      end
     end
   end
 
