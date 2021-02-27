8fd3dc78abf18d38c931c359209c6f3541ec1619
hasyrails <hasyrails@gmail.com>
2021-02-27 21:46:58 +0900

    動画検索結果表示ページ > 検索キーワードのページング

--- app/controllers/youtube/myvideos_controller.rb
+++ app/controllers/youtube/myvideos_controller.rb
@@ -2,5 +2,15 @@ class Youtube::MyvideosController < ApplicationController
   def index
     q = params[:q]
     @my_videos = YoutubeVideo.all.where(status: 'default').where('title LIKE ?', '%'+q+'%')
+
+    search_words = []
+    @search_words = []
+    youtube_videos = YoutubeVideo.order(created_at: "ASC").limit(5)
+
+    youtube_videos.each do |youtube_video|
+      search_word = youtube_video.search_keyword
+      search_words << search_word
+      @search_words = search_words.uniq
+    end
   end
 end
