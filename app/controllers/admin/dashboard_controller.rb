5fb37c49656f0b77992fb96fe9a1e1dcb65c90c8
hasyrails <hasyrails@gmail.com>
2021-03-17 21:56:56 +0900

    before_action :admin_user追加

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,22 +1,9 @@
 class Admin::DashboardController < ApplicationController
   before_action :admin_user
 
-  def index
-    # @table_names = ActiveRecord::Base.connection.tables.map{|t| t.classify}
-    
-    # @table_names = @table_names.drop(2)
-    
-    table_names = ActiveRecord::Base.connection.tables.drop(2)
-    @columns = []
-    table_names.each do |table_name|
-      @columns << ActiveRecord::Base.connection.columns(table_name)
-    end
-
-  end
-
   private
 
   def admin_user
-    redirect_to(root_path) unless current_user.admin?
-  end 
+    redirect_to(root_path) if  current_user.nil? || !current_user.admin?
+  end
 end
