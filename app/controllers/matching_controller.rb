a5fa73e0a9f8af21be5fe550141a91fe7432a6ad
hasyrails <hasyrails@gmail.com>
2021-03-17 22:32:26 +0900

    チャットルーム周りの修正

--- app/controllers/matching_controller.rb
+++ app/controllers/matching_controller.rb
@@ -4,7 +4,7 @@ class MatchingController < ApplicationController
   def index
     got_reaction_user_ids = Reaction.where(to_user_id: current_user.id, status: 'like').pluck(:from_user_id)
     
-    @match_users = Reaction.where(to_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
+    @match_users = Reaction.where(from_user_id: got_reaction_user_ids, from_user_id: current_user.id, status: 'like').map(&:to_user)
     @match_users = Kaminari.paginate_array(@match_users).page(params[:page]).per(5)
     
     @user = User.find(current_user.id)
