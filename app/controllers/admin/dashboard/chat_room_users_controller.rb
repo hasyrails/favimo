f8f1dfee69d01a406c3f3ba200c6a9666abffd18
hasyrails <hasyrails@gmail.com>
2021-03-18 06:28:50 +0900

    管理者画面（ChatRoomUserモデルレコード操作画面実装）

---
+++ app/controllers/admin/dashboard/chat_room_users_controller.rb
@@ -0,0 +1,58 @@
+class Admin::Dashboard::ChatRoomUsersController < ApplicationController
+  before_action :admin_user
+  before_action :set_chat_room_user_model_name
+  before_action :set_column_names_of_chat_room_user_model
+
+  def index
+    @chat_room_users = ChatRoomUser.page(params[:page]).per(5)
+  end
+
+  def show
+    @chat_room_user = ChatRoomUser.find(params[:id])
+  end
+
+  def edit
+    @chat_room_user = ChatRoomUser.find(params[:id])
+  end
+
+  def update
+    @chat_room_user = ChatRoomUser.find(params[:id])
+    @chat_room_user.update(chat_room_user_params)
+  end
+
+
+  
+  def destroy
+    @chat_room_user = ChatRoomUser.find(params[:id])
+    @chat_room_user.destroy
+    redirect_to admin_dashboard_chat_room_users_path
+  end
+
+
+
+  private
+  
+  def set_chat_room_user_model_name
+    @model_name = ChatRoomUser.model_name.name
+  end
+  
+  def set_column_names_of_chat_room_user_model
+    @columns = ActiveRecord::Base.connection.columns(:chat_room_users)
+    
+    column_names = []
+    @columns.each do |column|
+      column_names << column.name
+    end
+    @column_names = column_names
+
+  end
+  
+  def chat_room_user_params
+    params.require(:chat_room_user).permit(@column_names)
+  end
+
+  def admin_user
+    redirect_to(root_path) if  current_user.nil? || !current_user.admin?
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,7 @@ Rails.application.routes.draw do
       resources :users, controller: 'dashboard/users'
       resources :youtube_videos, controller: 'dashboard/youtube_videos'
       resources :chat_rooms, controller: 'dashboard/chat_rooms'
+      resources :chat_room_users, controller: 'dashboard/chat_room_users'
       resources :chat_messages, controller: 'dashboard/chat_messages'
       resources :reactions, controller: 'dashboard/reactions'
       resources :favorites, controller: 'dashboard/favorites'
