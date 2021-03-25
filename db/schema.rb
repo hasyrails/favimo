b9d04615ca52e547d2f4731d85207ad9ef19732d
hasyrails <hasyrails@gmail.com>
2021-03-26 06:40:41 +0900

    schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_16_211328) do
+ActiveRecord::Schema.define(version: 2021_03_23_220653) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_03_16_211328) do
   create_table "chat_rooms", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "status"
   end
 
   create_table "favorites", force: :cascade do |t|
