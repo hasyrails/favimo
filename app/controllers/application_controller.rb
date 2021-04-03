f2d5c7fa29fb3aa259bf576ad9fca5577fdbdb67
hasyrails <hasyrails@gmail.com>
2021-04-03 18:48:35 +0900

    継承メソッド set_table_namesをコメントアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
 
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :set_table_names
+  # before_action :set_table_names
   before_action :set_column_names
   
   protected
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :self_introduction, :profile_image])
   end
   
-  def set_table_names
-    @table_names = ActiveRecord::Base.connection.tables.map{|t| t.classify}
+  # def set_table_names
+  #   @table_names = ActiveRecord::Base.connection.tables.map{|t| t.classify}
       
-    @table_names = @table_names.drop(2)
-  end
+  #   @table_names = @table_names.drop(2)
+  # end
 
   def set_column_names
     table_names = ActiveRecord::Base.connection.tables.drop(2)
