ded0f7d66f4c072a0eebbc97ec3cccc8048dc734
hasyrails <hasyrails@gmail.com>
2021-04-03 18:52:17 +0900

    データベース　外部キー修正

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -1,6 +1,6 @@
 class Reaction < ApplicationRecord
-  belongs_to :to_user, class_name: "User"
-  belongs_to :from_user, class_name: "User"
+  belongs_to :to_user, class_name: "User", foreign_key: :to_user_id
+  belongs_to :from_user, class_name: "User", foreign_key: :from_user_id
 
   enum status: { like: 0, dislike: 1 }
 end
