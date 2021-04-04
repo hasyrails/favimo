39488165568fc94561c7cf7543f6a98be88ce582
hasyrails <hasyrails@gmail.com>
2021-04-04 10:34:06 +0900

    RSpec導入

---
+++ spec/support/login_support.rb
@@ -0,0 +1,10 @@
+module LoginSupport
+  # 利用者がログインする
+  def login_as(user)
+    visit root_path
+    click_link "Sign in"
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+    click_button "Log in"
+  end
+end
