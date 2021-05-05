31306433e7d0ad13e1b98c88fa475c454a90193e
hasyrails <hasyrails@gmail.com>
2021-05-06 06:09:46 +0900

    ChatMessageモデルにstatusカラムを追加

--- app/models/chat_message.rb
+++ app/models/chat_message.rb
@@ -2,5 +2,7 @@ class ChatMessage < ApplicationRecord
   belongs_to :user
   belongs_to :chat_room
 
+  enum status: { default: 0, dummy: 1 }
+
   after_create_commit { ChatMessageBroadcastJob.perform_later self }
 end
