0cadc9c8a1c2fecda06697e8332cc7281ea3f1b8
hasyrails <hasyrails@gmail.com>
2021-05-06 04:28:48 +0900

    誤字修正 dammy → dummy Favimo → faviMo

--- db/seeds/dummy_users.rb
+++ db/seeds/dummy_users.rb
@@ -2,12 +2,12 @@ p "create dummy users"
 
 5.times do |n|
   User.create!(
-    email: "dummy#{n + 1}@test.com",
+    email: "dummy#{n + 1}@mail.com",
     name: "dummy-user#{n + 1}",
-    password: "dummy_user",
+    password: "dummy-password",
     self_introduction: "dummy-user#{n+1}です",
     gender: "#{["male", "female"].sample}",
     profile_image: File.open("#{Rails.root}/db/dummy_images/#{rand(1..6)}.jpg"),
-    role: "dammy"
+    role: "dummy"
   )
 end
