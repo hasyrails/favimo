491ee653b0bde7b6ec8bc1cc35cf1426e78585cd
hasyrails <hasyrails@gmail.com>
2021-05-07 10:47:54 +0900

    通知メールの送信サーバー設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,7 +32,20 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: 'smtp.gmail.com',
+    # :user_name => "faviMo事務局",
+    user_name: Rails.application.credentials.gmail[:user_name],
+    # :password => "ycvfcaoiscrccxxn",
+    password: Rails.application.credentials.gmail[:password],
+    authentication: 'login'
+  }
 
   config.action_mailer.perform_caching = false
 
